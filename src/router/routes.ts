58931d5a51733ed1d26405953ba1eb1edec379a1
HosseinPourghadiri <hossein.654321@yahoo.com>
2024-01-04 11:17:14 +0330

    14021014

---
+++ src/router/routes.ts
@@ -0,0 +1,28 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/dashboard',
+    component: () => import('layouts/dashboard/DashboardLayout.vue'),
+    children: [
+      {
+        path: 'index',
+        component: () => import('pages/dashboard/IndexPage.vue')
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
